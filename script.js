4e4b52c998d6cb638b857a315698aa3fb0fc4da3
yishuenlo <lo.yishuen@gmail.com>
2020-06-29 20:04:00 -0600

    clean up code

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-const tenBtn = document.querySelector('.ten');
 const thirtyBtn = document.querySelector('.thirty');
-
-// parts to form search links
-let linkFront = 'https://www.bing.com/search?q=';
-let linkEnd = '&go=Search&qs=ds&form=QBRE';
-=======
-const thirtyBtn = document.querySelector(".thirty");
->>>>>>> gh-pages
 
 // keywords for search
 const cats = [
@@ -55,43 +46,6 @@ const cats = [
   "York chocolate cat",
 ];
 
-<<<<<<< HEAD
-// an array to store links
-let searchLinks = searchKeywords.map(link => linkFront + link + linkEnd);
-
-
-
-// initiate state of excution 
-let excuted = false;
-let currentTime;
-
-
-//start auto search at certain time, 11am
-//know current time
-//if function searching has been excuted, turn off auto search
-//else, keep auto search on
-setInterval(function() {
-    //grab fresh date at interval
-    let today = new Date();
-    console.log(`${today.getHours()}:${today.getMinutes()}`);
-
-    //set current time at interval
-    currentTime = `${today.getHours()}:${today.getMinutes()}`;
-
-    //if current time is 11am and function has not been excuted
-    if (currentTime == '7:5' && excuted == false) {
-        //start searching
-        searching(searchLinks, 3);
-    }
-
-    //if current time is 10pm and function has been excuted aleady,
-    if (currentTime == '22:0' && excuted == true) {
-        //refresh and set excuted to false
-        excuted = false;
-    }
-}, 60000);
-
-=======
 const animals = [
   "cat",
   "tuxedo cat",
@@ -136,54 +90,18 @@ const animals = [
 let searchLinks = animals.map(
   (keyword) => `https://www.bing.com/images/search?q=silly ${keyword}`
 );
->>>>>>> gh-pages
 
-//----------- excution of searching -----------
 // open and closing tabs
 function automateTabs(link) {
-<<<<<<< HEAD
-    let tab = window.open(link, '_blank');
-    // close tab after 2000ms
-    setTimeout(function () {
-        tab.close();
-    }, 5000)
-=======
   const tab = window.open(link, "_blank");
   // close tab after 5000ms
   setTimeout(function () {
     tab.close();
   }, 5000);
->>>>>>> gh-pages
 }
 
 // function to run open and closing tabs in sequence
 function searching(arr, iterate) {
-<<<<<<< HEAD
-    // run first link first
-    automateTabs(arr[0]);
-
-    //run subsequent links after specific timelag
-    for (let i = 1; i < iterate; i++) {
-        setTimeout(function () {
-            automateTabs(arr[i]);
-        }, timelag * i);
-    }
-
-    excuted = true;
-};
-
-tenBtn.addEventListener('click', function () { searching(searchLinks, 10);});
-thirtyBtn.addEventListener('click', function () { searching(searchLinks, 34);});
-
-//keep track of how long the tab has been open
-//if it's longer than an hour, and excuted = false, and it's in the afternoon (hour > 11)
-//run searching function 
-
-
-//if the date is different, and function had been run already, reset
-//if today's date !== excuted date && excuted = true;
-//reset excuted to false;
-=======
   // set time lag for subsquent links
   const timelag = 6000;
   // run first link first
@@ -200,4 +118,4 @@ thirtyBtn.addEventListener('click', function () { searching(searchLinks, 34);});
 thirtyBtn.addEventListener("click", function () {
   searching(searchLinks, 34);
 });
->>>>>>> gh-pages
+
